0929d7c4791254e5aaaa7187368704a166a5dfc7
webkovacs <webkovacs@gmail.com>
2023-01-23 21:49:17 +0100

    nevezéktan miatti refact

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
@@ -1,23 +1,24 @@
 package hu.kovacspeterzoltan.bootcamp.vehiclestarter;
 
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.controller.ConsoleUIController;
-import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.dao.ConsoleUIRegisterPresenterImp;
+import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.dto.ConsoleUIRegisterPresenterImp;
 import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorageCSV;
-import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleRegister;
-import hu.kovacspeterzoltan.bootcamp.vehicleregister.storage.VehicleRegisterStorage;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleInteractor;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.api.VehicleRegisterPresenterInterface;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.storage.VehicleRegisterStorageInterface;
 
 public class Main {
     public static void main(String[] args) {
-        VehicleRegister registerInteractor = new VehicleRegister();
+        VehicleInteractor vehicleInteractor = new VehicleInteractor();
 
-        VehicleRegisterStorage persistenceStorageCSV = new PersistenceStorageCSV();
-        registerInteractor.setStorageImp(persistenceStorageCSV);
+        VehicleRegisterStorageInterface persistenceStorageCSV = new PersistenceStorageCSV();
+        vehicleInteractor.setStorageImp(persistenceStorageCSV);
 
-        ConsoleUIRegisterPresenterImp consoleUIRegisterPresenter = new ConsoleUIRegisterPresenterImp();
-        registerInteractor.setPresenterImp(consoleUIRegisterPresenter);
+        VehicleRegisterPresenterInterface consoleUIRegisterPresenter = new ConsoleUIRegisterPresenterImp();
+        vehicleInteractor.setPresenterImp(consoleUIRegisterPresenter);
 
         ConsoleUIController consoleUI = new ConsoleUIController();
-        consoleUI.setRegisterInteractorImp(registerInteractor);
+        consoleUI.setRegisterInteractorImp(vehicleInteractor);
 
         consoleUI.start();
     }
