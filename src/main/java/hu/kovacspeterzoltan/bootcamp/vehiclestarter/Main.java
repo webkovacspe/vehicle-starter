6e97461abb8df0f89a9805d7aa60fe18837e7cf7
webkovacs <webkovacs@gmail.com>
2023-01-23 14:22:37 +0100

    ResponseJSON refact

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
@@ -3,7 +3,7 @@ package hu.kovacspeterzoltan.bootcamp.vehiclestarter;
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.controller.ConsoleUIController;
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.dao.ConsoleUIRegisterPresenterImp;
 import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorageCSV;
-import hu.kovacspeterzoltan.bootcamp.vehicleregister.controller.RegisterInteractor;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.interactor.RegisterInteractor;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.dao.VehicleRegisterStorageInterface;
 
 public class Main {
