ea84aaae5734688254304e994b55e0fddf743470
webkovacs <webkovacs@gmail.com>
2023-01-23 16:40:26 +0100

    verzióemelés és nevezéktan miatti refact

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
@@ -3,14 +3,14 @@ package hu.kovacspeterzoltan.bootcamp.vehiclestarter;
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.controller.ConsoleUIController;
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.dao.ConsoleUIRegisterPresenterImp;
 import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorageCSV;
-import hu.kovacspeterzoltan.bootcamp.vehicleregister.interactor.RegisterInteractor;
-import hu.kovacspeterzoltan.bootcamp.vehicleregister.dao.VehicleRegisterStorageInterface;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleRegister;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.storage.VehicleRegisterStorage;
 
 public class Main {
     public static void main(String[] args) {
-        RegisterInteractor registerInteractor = new RegisterInteractor();
+        VehicleRegister registerInteractor = new VehicleRegister();
 
-        VehicleRegisterStorageInterface persistenceStorageCSV = new PersistenceStorageCSV();
+        VehicleRegisterStorage persistenceStorageCSV = new PersistenceStorageCSV();
         registerInteractor.setStorageImp(persistenceStorageCSV);
 
         ConsoleUIRegisterPresenterImp consoleUIRegisterPresenter = new ConsoleUIRegisterPresenterImp();
