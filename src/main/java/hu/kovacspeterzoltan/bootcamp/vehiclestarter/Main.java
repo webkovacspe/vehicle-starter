48921c8ae128e77f54aee3029c30aa800649b1ff
webkovacs <webkovacs@gmail.com>
2023-01-25 23:08:11 +0100

    API interface szétválasztása

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclestarter/Main.java
@@ -2,7 +2,7 @@ package hu.kovacspeterzoltan.bootcamp.vehiclestarter;
 
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.controller.ConsoleUIController;
 import hu.kovacspeterzoltan.bootcamp.vehicleconsoleui.dto.ConsoleUIRegisterPresenterImp;
-import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorageCSV;
+import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.VehicleRegisterPersistenceStorageCSV;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleInteractor;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.api.VehicleRegisterPresenterInterface;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.storage.VehicleRegisterStorageInterface;
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         VehicleInteractor vehicleInteractor = new VehicleInteractor();
 
-        VehicleRegisterStorageInterface persistenceStorageCSV = new PersistenceStorageCSV();
+        VehicleRegisterStorageInterface persistenceStorageCSV = new VehicleRegisterPersistenceStorageCSV();
         vehicleInteractor.setStorageImp(persistenceStorageCSV);
 
         VehicleRegisterPresenterInterface consoleUIRegisterPresenter = new ConsoleUIRegisterPresenterImp();
